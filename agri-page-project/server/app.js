9b2616b0647fd63d00590b4a1eeb57a156f8fdd7
hassan sabeh <h.adamsabeh@yahoo.com>
2021-12-04 18:17:00 +0100

    WIP views/index

--- agri-page-project/server/app.js
+++ agri-page-project/server/app.js
@@ -9,15 +9,26 @@ require("./db");
 // https://www.npmjs.com/package/express
 const express = require("express");
 
+//importing handlerbars for views
+const hbs = require("hbs");
+const path = require("path")
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+//to be moved to the configuration file?
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
+//mointing the index routes middleware
+const index = require("./routes/index");
+app.use("/", index);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- agri-page-project/server/routes/index.js
+++ agri-page-project/server/routes/index.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+// router.get("/", (req, res, next) => {
+//   res.json("All good in here");
+// });
 
 // You put the next routes here 👇
 // example: router.use("/auth", authRoutes)
+
+router.get("/", function(req, res, next) {
+  res.render('index');
+})
 
 module.exports = router;
