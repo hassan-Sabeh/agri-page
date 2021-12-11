e500fade78db8e6b32f5a3756a751c0c2876b6a2
hassan sabeh <h.adamsabeh@yahoo.com>
2021-12-11 12:02:15 +0100

    routes done for testing

--- agri-page-project/server/app.js
+++ agri-page-project/server/app.js
@@ -35,6 +35,9 @@ app.use('/', login);
 const signup = require('./routes/signup');
 app.use('/', signup);
 
+const profile = require("./routes/profile");
+app.use('/', profile);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
