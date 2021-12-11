5f148e99323564ed9e62fbeededed179b23375a6
Cleiton1912 <cleiton.silvalima@outlook.fr>
2021-12-11 16:00:21 +0100

    signupfarmer

--- agri-page-project/server/routes/signup.js
+++ agri-page-project/server/routes/signup.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.get('/signuptransition', (req, res, next) => {
+    res.render('auth/signuptransition');
+})
+
 router.get('/signupclient', (req, res, next) => {
     res.render('auth/signupclient');
 })
