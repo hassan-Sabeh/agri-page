c2e4145678d658a3a479f5ed82c2890901065382
hassan sabeh <h.adamsabeh@yahoo.com>
2021-12-16 19:51:51 +0100

    WIP profile farmer

--- agri-page-project/server/routes/profile.js
+++ agri-page-project/server/routes/profile.js
@@ -1,11 +1,40 @@
 const router = require("express").Router();
 
+const businessInfor = [
+    {
+        businessName: "agri business",
+        region: "agri region"
+    },
+    {
+        businessName: "agri business",
+        region: "agri region"
+    },
+    {
+        businessName: "agri business",
+        region: "agri region"
+    },
+    {
+        businessName: "agri business",
+        region: "agri region"
+    },
+    {
+        businessName: "agri business",
+        region: "agri region"
+    },
+    {
+        businessName: "agri business",
+        region: "agri region"
+    },
+]
+
+
+
 router.get('/profile', (req, res, next) => {
     res.render("profile");
 });
 
 router.get('/profilebusiness', (req, res, next) => {
-    res.render("profilebusiness");
+    res.render("profilebusiness", {businessInfo: businessInfor});
 });
 
 
